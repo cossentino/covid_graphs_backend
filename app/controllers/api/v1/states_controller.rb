83bdab462532a604d4090da60c8d3302e40ab6f7
Ian Cossentino <icossentino@gmail.com>
2021-03-07 16:41:49 -0500

    added index controller method for states

--- app/controllers/api/v1/states_controller.rb
+++ app/controllers/api/v1/states_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::StatesController < ApplicationController
+
+  def index
+    states = State.all
+    render json: states
+  end
+
+
+
+
+
+
+  private
+
+
+  def states_params
+    params.require(:state).permit(:name, :region, :population)
+  end
+
 end
