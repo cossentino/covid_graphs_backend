d93e5b0799d5d1959ef3104bbcfcd0b7d8d7f4a2
Ian Cossentino <icossentino@gmail.com>
2021-03-09 13:31:15 -0500

    continuing data cleaning and entry

---
+++ db/data_cleaning.rb
@@ -0,0 +1,20 @@
+require "csv"
+
+
+recent_counties_path = "/Users/iancossentino/Development/code/Mod4/4_project/Data/us-counties-recent.csv"
+cleaned_file_path = "/Users/iancossentino/Development/code/Mod4/4_project/Data/us-counties-recent-cleaned.csv"
+
+
+
+def add_filler_fips_to_unknowns(csv)
+  csv = CSV.read(csv)
+  csv.each do |e|
+    if e[2] == "Unknown"
+      s = State.find_by(name: e[2])
+    e[-3] = "#{s.fips_prefix}000"
+    puts e
+    end
+  end
+end
+
+add_filler_fips_to_unknowns(recent_counties_path)
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,41 @@ require "csv"
 
 statesFile = "/Users/iancossentino/Development/code/Mod4/4_project/Data/nst-est2019-alldata.csv"
 countiesFile = "/Users/iancossentino/Development/code/Mod4/4_project/Data/us-counties.csv"
+
 state_fips_prefix_file = "/Users/iancossentino/Development/code/Mod4/4_project/Data/fips_prefixes.csv"
+
+# Link counties to states via fips codes
+
+def link_counties_to_states(csv)
+  csv = CSV.read(csv)
+  csv.delete_at(0)
+  csv.each do |e|
+    if e[1] != "Unknown"
+      prefix = e[3][0..1]
+      s = State.find_by(fips_prefix: prefix)
+      c = s.counties.build(name: e[1], fips: e[3].to_i)
+      if c.save
+        puts c.name
+      else
+        puts "EEEEROOOOOOOOR"
+      end
+    elsif e[1] == "Unknown"
+      s = State.find_by(name: e[2])
+      c = s.counties.build(name: "Unknown", fips: "#{s.fips_prefix}000".to_i)
+      if c.save
+        puts c.name
+      else
+        puts "UNKNOWN WAS NOT SAVED"
+      end
+    end
+  end
+end
+
+link_counties_to_states("/Users/iancossentino/Development/code/Mod4/4_project/Data/us-counties-recent.csv")
+
+
+
+
 
 # Add State names + population to database
 
@@ -38,17 +72,42 @@ state_fips_prefix_file = "/Users/iancossentino/Development/code/Mod4/4_project/D
 
 
 
-def add_fips_prefixes_to_states(csv)
-  csv = CSV.read(csv)
-  csv.each do |e|
-    s = State.find_by(name: e[0])
-    if s
-      s.update(fips_prefix: e[2])
-    end
-  end
-end
 
-add_fips_prefixes_to_states(state_fips_prefix_file)
+# Add county names + fips codes to database
+
+# def create_states_with_population_array(csv)
+#   csv = CSV.read(csv)
+#   csv = csv[6..-1]
+#   statesArray = []
+#   csv.each do |e|
+#     if !statesArray.include?(e[4])
+#       statesArray << [e[4], e[5]]
+#     end
+#   end
+#   statesArray.each do |e|
+#     s = State.new(name: e[0], population: e[1])
+#     if s.save
+#       puts s.name
+#     else
+#       puts "error"
+#     end
+#   end
+# end
+
+
+
+
+# def add_fips_prefixes_to_states(csv)
+#   csv = CSV.read(csv)
+#   csv.each do |e|
+#     s = State.find_by(name: e[0])
+#     if s
+#       s.update(fips_prefix: e[2])
+#     end
+#   end
+# end
+
+# add_fips_prefixes_to_states(state_fips_prefix_file)
 
 # create_states_with_population_array(statesFile)
 
