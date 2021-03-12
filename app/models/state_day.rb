ec76a2dfd47a918f6554474d12ce9439ea6cab08
Ian Cossentino <icossentino@gmail.com>
2021-03-12 16:24:58 -0500

    minor refactor

--- app/models/state_day.rb
+++ app/models/state_day.rb
@@ -1,4 +1,6 @@
 class StateDay < ApplicationRecord
   belongs_to :state
   validates :date, uniqueness: { scope: :state_id }
+  validates :date, presence: true
+  validates :cases, presence: true
 end
