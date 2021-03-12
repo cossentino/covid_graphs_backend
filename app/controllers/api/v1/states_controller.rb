fba55bfb541d20b28ce171592f5e1da07ec2686e
Ian Cossentino <icossentino@gmail.com>
2021-03-12 09:52:54 -0500

    added update method to states controller to update total cases

--- app/controllers/api/v1/states_controller.rb
+++ app/controllers/api/v1/states_controller.rb
@@ -7,8 +7,17 @@ class Api::V1::StatesController < ApplicationController
 
 
   def show
-    state = State.find(params[:id])
+    state = State.find_by(id: params[:id])
     render json: StatesSerializer.new(state)
+  end
+
+  def update
+    state = State.find_by(id: params[:id])
+    puts state.total_cases
+    if state.update(total_cases: state.total_cases + params[:total_cases].to_i)
+      render json: "rendering", status: :accepted
+    else
+      render json: {errors: sd.errors.full_messages}, status: :unprocessible_entity
   end
 
 
@@ -17,7 +26,7 @@ class Api::V1::StatesController < ApplicationController
 
 
   def states_params
-    params.require(:state).permit(:name, :region, :population)
+    params.require(:state).permit(:name, :region, :population, :total_cases)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :states, only: [:index, :show] do
+      resources :states, only: [:index, :show, :update] do
         resources :state_days, only: [:index, :create]
         resources :counties, only: [:index]
       end
